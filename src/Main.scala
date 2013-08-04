450140894529e86a8b95314ac62ee2e048fe0b75
tatsuo1234@gamma.ocn.ne.jp <tatsuo1234@gamma.ocn.ne.jp>
2013-08-04 19:55:47 +0900

    Simple date format

--- src/Main.scala
+++ src/Main.scala
@@ -3,15 +3,49 @@ import Animals.Animals
 object Main {
 
   def main(args:Array[String]) = {
-    val l = "全体,ぜんたい"
-    println(l.substring(0, l.indexOf(",")), l.substring(l.indexOf(",") + 1))
+    simpleDateFormatTest
   }
+  
+  /**
+   * Simple date format
+   */
+  def simpleDateFormatTest() {
+    simpleDateFormat(5000)     //0:05  
+    simpleDateFormat(100000)   //1:40
+    simpleDateFormat(1000000)  //16:40
+    simpleDateFormat(10000000) //2:46:40
+  }
+  def simpleDateFormat(milliseconds: Long = 9400000) {
+    val df = new java.text.SimpleDateFormat("H:mm:ss")
+    df.setTimeZone(java.util.TimeZone.getTimeZone("UTC"))
+    val hms = df.format(milliseconds)
+    
+    val pattern = """(\d+):(\d+):(\d+)""".r
+    
+    val result = hms match {
+      case pattern(h, mm, ss) if h == "0" && mm.head == '0' => mm.last.toString + ":" + ss
+      case pattern(h, mm, ss) if h == "0"                   => mm + ":" + ss
+      case _ => hms
+    }
+    
+    println(result)
+  }
+  
+  /**
+   * String split sample
+   */
+  def splitString() {
+    //val l = "全体,ぜんたい"
+    //println(l.substring(0, l.indexOf(",")), l.substring(l.indexOf(",") + 1))
+    val s = "音声ファイル01.mp3"
+    println(s.substring(0, s.lastIndexOf('.')))
+  } 
   
   /**
    * List shuffle sample
    */
   def randomList() {
-    val l = List.range(1, 100 + 1)
+    val l = 1 to 100 //List.range(1, 100 + 1)
     println(l)
     //ランダムに4つ取り出し
     println(scala.util.Random.shuffle(l).take(4))
